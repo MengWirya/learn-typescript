e84f74a0196020ad0441f82d52b419cc5b5da7ee
MengWirya <mengwirya@gmail.com>
2026-07-28 13:56:39 +0700

    completing data types task 3 and 4

--- src/01-data-types/tasks/task-03.ts
+++ src/01-data-types/tasks/task-03.ts
@@ -10,18 +10,54 @@
  * display the student data using console.log.
  */
 
-import promptSync from 'prompt-sync';
-const prompt = promptSync();
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
 
-let student: {studentID: string, studentName: string, studentAge: number, studentStatus: boolean} = {
-    studentID: prompt(`masukan ID anda:`),
-    studentName: prompt(`masukan nama anda:`),
-    studentAge: Number(prompt(`masukan umur anda:`)),
-    studentStatus: Boolean(prompt(`status murid: `))
-};
+interface student {studentID: string, studentName: string, studentAge: number, studentStatus: boolean}
 
-console.log(`Deskripsi Murid`);
-console.log(`ID Murid ${student.studentID}`);
-console.log(`Nama Murid ${student.studentName}`);
-console.log(`Umur Murid ${student.studentAge}`);
-console.log(`Status Murid ${student.studentStatus}`);
+let studentList: student[] = [];
+
+for (let i = 1; i <= 3; i++) {
+    const idInput = prompt(`Masukan ID siswa milikmu: `);
+    const nameInput = prompt(`Masukan namamu: `);
+    const ageInput = Number(prompt(`Masukan umurmu: `));
+
+    let statusInput = ""
+    while (true) {
+        const booleanInput = prompt(`Status siswa (True/False): `);
+        statusInput = booleanInput ? booleanInput.toLowerCase().trim() : "";
+
+        if (statusInput === "true" || statusInput === "false") {
+            break;
+        }
+        alert("Input salah! Anda hanya boleh mengetik 'true' atau 'false'.");
+    }
+
+    let newStudent: student = {
+        studentID: idInput ?? "",
+        studentName: nameInput ?? "",
+        studentAge: ageInput ?? "",
+        studentStatus: statusInput === "true",
+    }
+
+    studentList.push(newStudent);
+}
+
+console.log(`List siswa: ${studentList}`);
+
+// ----------- TESTING -----------
+// import promptSync from 'prompt-sync';
+// const prompt = promptSync();
+
+// let student: {studentID: string, studentName: string, studentAge: number, studentStatus: boolean} = {
+//     studentID: prompt(`masukan ID anda:`),
+//     studentName: prompt(`masukan nama anda:`),
+//     studentAge: Number(prompt(`masukan umur anda:`)),
+//     studentStatus: Boolean(prompt(`status murid: `).toLowerCase())
+// };
+
+// console.log(`Deskripsi Murid`);
+// console.log(`ID Murid ${student.studentID}`);
+// console.log(`Nama Murid ${student.studentName}`);
+// console.log(`Umur Murid ${student.studentAge}`);
+// console.log(`Status Murid ${student.studentStatus}`);
--- src/01-data-types/tasks/task-04.ts
+++ src/01-data-types/tasks/task-04.ts
@@ -11,6 +11,34 @@
  * display the book data using console.log.
  */
 
-let book: {bookISBNNumber: string, bookTitle: string, bookAuthor: string, bookPages: number} = {
-    bookISBNNumber: `01231525`,
+import PromptSync = require("prompt-sync");
+const prompt = PromptSync();
+
+interface book {bookISBNNumber: string, bookTitle: string, bookAuthor: string, bookPages: number, bookCategory: string};
+
+let bookList: book[] = [];
+
+for (let i = 1; i <= 3; i++) {
+    const inputISBNNumber = prompt(`Masukan ISBN Buku: `);
+    const inputTitle = prompt(`Masukan judul buku: `);
+    const inputAuthor = prompt(`Masukan author buku: `);
+    const inputPages = Number(prompt(`Masukan jumlah halaman buku: `));
+    const inputCategory = prompt(`Masukan kategori buku: `);
+
+    let newBook: book = {
+        bookISBNNumber: inputISBNNumber ?? "",
+        bookTitle: inputTitle ?? "",
+        bookAuthor: inputAuthor ?? "",
+        bookPages: inputPages ?? "",
+        bookCategory: inputCategory ?? "",
+    }
+    
+    bookList.push(newBook);
 }
+
+console.log(`Book list: ${bookList}`);
+
+// enum category {
+//     Fiction = "FICTION",
+//     NonFiction = "NONFICTION"
+// }
